c1e03f32f47b7f220aceb8985b0ce7507897b051
Doriane-Lami <doriane.lami@etud.univ-jfc.fr>
2022-10-19 11:00:42 +0200

    Classe Jiwoo

---
+++ Jiwoo.java
@@ -0,0 +1,10 @@
+
+    public class Jiwoo {
+        private String nom;
+        private String prenom;
+
+        public Jiwoo(String n, String p) {
+            nom =n;
+            prenom=p;
+        }
+    }
